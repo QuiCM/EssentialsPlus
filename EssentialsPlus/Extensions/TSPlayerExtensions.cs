1f82a2bfc3aaa72635d26aafbbf839dfa23ab41a
MarioE <marioe27@hotmail.com>
2014-07-26 19:56:13 -0400

    Accidentally left out a few files

---
+++ EssentialsPlus/Extensions/TSPlayerExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+
+namespace EssentialsPlus.Extensions
+{
+	public static class TSPlayerExtensions
+	{
+		private static Dictionary<TSPlayer, Player> players = new Dictionary<TSPlayer, Player>();
+
+		public static void AttachEssentialsPlayer(this TSPlayer tsplayer)
+		{
+			if (!players.ContainsKey(tsplayer))
+				players.Add(tsplayer, new Player(tsplayer));
+		}
+		public static void DetachEssentialsPlayer(this TSPlayer tsplayer)
+		{
+			players.Remove(tsplayer);
+		}
+		public static Player GetEssentialsPlayer(this TSPlayer tsplayer)
+		{
+			if (!players.ContainsKey(tsplayer))
+				return null;
+			return players[tsplayer];
+		}
+		public static bool HasPermission(this TSPlayer tsplayer, string permission)
+		{
+			return tsplayer.Group != null && tsplayer.Group.HasPermission(permission);
+		}
+	}
+}
---
+++ EssentialsPlus/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EssentialsPlus.Extensions;
+using TShockAPI;
+
+namespace EssentialsPlus
+{
+	public class Player
+	{
+		private List<Vector2> backPoints = new List<Vector2>();
+
+		public ReadOnlyCollection<Vector2> BackPoints
+		{
+			get { return backPoints.AsReadOnly(); }
+		}
+		public int Index
+		{
+			get { return TSPlayer.Index; }
+		}
+		public Terraria.Player TPlayer
+		{
+			get { return TSPlayer.TPlayer; }
+		}
+		public TSPlayer TSPlayer { get; private set; }
+
+		public Player(TSPlayer tsplayer)
+		{
+			TSPlayer = tsplayer;
+		}
+
+		public void AddBackPoint(Vector2 vector)
+		{
+			backPoints.Insert(0, vector);
+			if (backPoints.Count > EssentialsPlus.Config.BackPointHistory)
+				backPoints.RemoveAt(backPoints.Count - 1);
+		}
+		public bool HasPermission(string permission)
+		{
+			return TSPlayer != null && TSPlayer.HasPermission(permission);
+		}
+		public void SendErrorMessage(string format, params object[] args)
+		{
+			if (TSPlayer != null)
+				TSPlayer.SendErrorMessage(format, args);
+		}
+		public void SendInfoMessage(string format, params object[] args)
+		{
+			if (TSPlayer != null)
+				TSPlayer.SendInfoMessage(format, args);
+		}
+		public void SendSuccessMessage(string format, params object[] args)
+		{
+			if (TSPlayer != null)
+				TSPlayer.SendSuccessMessage(format, args);
+		}
+		public void SendWarningMessage(string format, params object[] args)
+		{
+			if (TSPlayer != null)
+				TSPlayer.SendWarningMessage(format, args);
+		}
+		public void Teleport(Vector2 vector)
+		{
+			if (TSPlayer != null)
+				TSPlayer.Teleport(vector.X, vector.Y);
+		}
+	}
+}
