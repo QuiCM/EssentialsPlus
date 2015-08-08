c55a370ca28e6e4d5f0595f2e89f3e7e79f1ae91
White <monsieur_white@hotmail.com>
2015-08-08 10:23:42 +0930

    API 1.21

--- EssentialsPlus/EssentialsPlus.cs
+++ EssentialsPlus/EssentialsPlus.cs
@@ -15,7 +15,7 @@ using TShockAPI.Hooks;
 
 namespace EssentialsPlus
 {
-	[ApiVersion(1, 20)]
+	[ApiVersion(1, 21)]
 	public class EssentialsPlus : TerrariaPlugin
 	{
 		public static Config Config { get; private set; }
