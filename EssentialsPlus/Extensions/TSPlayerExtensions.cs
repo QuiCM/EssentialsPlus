0aba150d693b8bca457b48d122040aac087760f1
Enerdy <rodrigo.rente@live.com>
2016-06-03 20:19:18 +0100

    API 1.23

--- EssentialsPlus/Extensions/TSPlayerExtensions.cs
+++ EssentialsPlus/Extensions/TSPlayerExtensions.cs
@@ -1,15 +1,14 @@
-﻿using System.Runtime.CompilerServices;
-using TShockAPI;
+﻿using TShockAPI;
 
 namespace EssentialsPlus.Extensions
 {
 	public static class TSPlayerExtensions
 	{
-		private static ConditionalWeakTable<TSPlayer, PlayerInfo> players = new ConditionalWeakTable<TSPlayer, PlayerInfo>();
-
 		public static PlayerInfo GetPlayerInfo(this TSPlayer tsplayer)
 		{
-			return players.GetOrCreateValue(tsplayer);
+			if (!tsplayer.ContainsData(PlayerInfo.KEY))
+				tsplayer.SetData(PlayerInfo.KEY, new PlayerInfo());
+			return tsplayer.GetData<PlayerInfo>(PlayerInfo.KEY);
 		}
 	}
 }
--- EssentialsPlus/Properties/AssemblyInfo.cs
+++ EssentialsPlus/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EssentialsPlus")]
-[assembly: AssemblyCopyright("Copyright © WhiteX et al. 2014-2015")]
+[assembly: AssemblyCopyright("Copyright © WhiteX et al. 2014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyFileVersion("1.1.3")]
